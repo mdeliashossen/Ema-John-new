a0d6f5836b9f160b6359ddbb8364e92a486b14e5
mdeliashossen <skeliascse@gmail.com>
2023-10-04 18:53:18 +0600

    set cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 const Cart = ({cart}) => {
     let totalPrice =0;
     let totalShipping = 0;
+    let quantity =0;
    for (const product of cart){
-        totalPrice = totalPrice + product.price;
+    if(product.quantity === 0){
+        product.quantity=1;
+    }
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = (totalPrice*7)/100;
     const grandTotal = totalPrice + tax;
+
    
     // console.log(cart);
     return (
         <div>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price : {totalPrice}</p>
             <p>Total Shipping : {totalShipping}</p>
             <p>Tax : {tax}</p>
